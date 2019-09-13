5c4615132352877065569a8ecb7781f1cf7ca328
Ollie89 <oliver.lehmann89@gmail.com>
2019-09-13 09:57:51 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -177,7 +177,7 @@ GLUON_TLWDR4900_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_P
 #		to decide if a version is newer or not.
 
 #DEFAULT_GLUON_RELEASE := 0.6+mstr$(shell date '+%Y%m%d')
-DEFAULT_GLUON_RELEASE := 201821LZTD
+DEFAULT_GLUON_RELEASE := 20182xLZTD
 
 
 ##	GLUON_RELEASE
@@ -188,7 +188,7 @@ DEFAULT_GLUON_RELEASE := 201821LZTD
 #			gluon-ff%site_code%-23.42+5-%router_model%.bin
 
 # Allow overriding the release number from the command line
-GLUON_RELEASE ?= 201821LZTD
+GLUON_RELEASE ?= 20182xLZTD
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
